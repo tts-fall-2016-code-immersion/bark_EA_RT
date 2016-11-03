98f3dab984e7920b2c78c8f77a85ae66ed549561
richthomas <richthomas324@gmail.com>
2016-11-02 21:07:52 -0400

    updated Tweet Controller, User Model, and Tweet Model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+
+  validates :message, presence: true, length: {maximum: 140, too_long: "A tweet is only 140 characters! Everybody knows that!"}
+
 end
