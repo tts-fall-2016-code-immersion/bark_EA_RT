1ee7643592188674f62f3e34ec3a50c9babc1e86
Earth287 <erin007ster@gmail.com>
2016-11-05 13:34:32 -0400

    added validation to username and message, added update, destroy and set_tweet to tweet controller, updated views, set index as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :tweets
   root 'tweets#index'
   devise_for :users
-end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
