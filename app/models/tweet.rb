43ad7ccadc8e19790d3fa20ede69f13ce98de817
Earth287 <erin007ster@gmail.com>
2016-11-09 18:54:31 -0500

    app complete, need to get it to work, tweets/index issues

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
-
+  has_many :likes
   validates :message, presence: true, length: {maximum: 140, too_long: "This is too long"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 
-  resources :tweets
+
+  get "profiles" => "profiles#index"
+  get 'profiles/:id' => 'profiles#show', as: :profile
+
+
+
+  resources :tweets do
+       resource :like
+   end
+
+  resources :relationships
   root 'tweets#index'
   devise_for :users
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -58,4 +69,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
